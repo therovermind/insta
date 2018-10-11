cdb689a1751a52c5837cd53792cb582ca308a2bd
ankur <ankursinghal.dev@gmail.com>
2018-10-11 14:18:27 +0530

    -a

--- src/components/InstaClone.js
+++ src/components/InstaClone.js
@@ -1,23 +1,26 @@
 import React,{ Component } from 'react';
 import { Text,View,Image,StyleSheet,Dimensions } from 'react-native';
+import config from '../config';
 
 export default class InstaClone extends Component{
 
     constructor(){
         super();
         this.state={
-            screenWidth:0
+            screenWidth: Dimensions.get("window").width
         };
     }
 
-    componentDidMount(){
-        this.setState({
-            screenWidth: Dimensions.get("window").width
-        })
-    }
+    
 
     render(){
+
+        const imageHieght = Math.floor(this.state.screenWidth*1.1);
+        const imageURI = "https://lh3.googleusercontent.com/jIORt-l8iFGHbK9RTLVTB19cRW7GgCav40USQn_-jpfsc-21JdhI-A-dBg4Ftt0O6N50WQNK1RBlgPBuT6G7-zUjKw"+ "=s" + imageHieght +"-c";
+            
+
         return(
+            
             <View style={{ flex:1, width:100 + '%',height:50 }}>
                 <View style={styles.NavigationTap}>
                   <Text>Instagram</Text>
@@ -25,18 +28,32 @@ export default class InstaClone extends Component{
                 <View style={styles.userBar}  >
                   <View style={{ alignItems:"center",flexDirection:"row"}}>
                      <Image style ={styles.userPic} 
-                         source={{uri:"https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426"}}
+                         source={{uri:"https://lh3.googleusercontent.com/jIORt-l8iFGHbK9RTLVTB19cRW7GgCav40USQn_-jpfsc-21JdhI-A-dBg4Ftt0O6N50WQNK1RBlgPBuT6G7-zUjKw=s1024"}}
                       />
                      <Text style={{marginLeft:7}} >
                         Ankur singhal
                      </Text>
                   </View>
-                  <View><Text style={{fontSize:30, alignItems:"center"}}>...</Text></View>            
+                  <View>
+                      <Text style={{fontSize:30, alignItems:"center"}}>...</Text>
+                  </View>            
                 </View>
                 <Image 
-                  style= { {  width:this.state.screenWidth, height:100  }} 
-                  source = {{uri:"https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426" }} 
+                  style= { {  width:this.state.screenWidth, height:imageHieght  }} 
+                  source = {{uri:imageURI }} 
                 />
+                <View style={styles.iconBar} >
+                  <Image style ={[styles.icon,{height:40,width:40}]}
+                         source={config.images.heartIcon} 
+                    />
+                  <Image style ={[styles.icon,{height:40,width:38}]} 
+                         source={config.images.bubbleIcon} 
+                    />
+                  <Image style ={[styles.icon,{height:38,width:38}]}
+                         source={config.images.arrowIcon} 
+                    />
+
+                </View>
             </View>
         )
     }
@@ -52,20 +69,35 @@ const styles = StyleSheet.create({
         borderBottomWidth: StyleSheet.hairlineWidth,
         borderBottomColor:'rgb(233,233,233)'
 
-},
-userBar:{
-    height:50,
+    },
+   userBar:{
+    height:config.stlyleConstant.rowHeight,
     width:100+'%',
     justifyContent:"space-between",
     paddingHorizontal:10,
     flexDirection:"row"
-},
+   },
 
-userPic:{
+   userPic:{
     height:40,
     width:40,
     borderRadius:25
-}
-});
+   },
+
+   iconBar:{
+       height: config.stlyleConstant.rowHeight,
+       borderBottomWidth : StyleSheet.hairlineWidth,
+       borderTopWidth: StyleSheet.hairlineWidth,
+       borderColor:"rgb(233,233,233)",
+       flexDirection:"row",
+       alignItems:"center"
+   },
+
+   icon:{
+       marginLeft:10,
+       
+       
+   }
+})
 
 
---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default{
+    images:{
+        heartIcon: require('../../assets/heart.png'),
+        arrowIcon: require('../../assets/arrow.png'),
+        bubbleIcon: require('../../assets/bubble.png')
+    },
+    stlyleConstant:{
+        rowHeight:50
+    }
+}
