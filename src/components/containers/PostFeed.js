73b7433745c91ed2693a88902dc7820d0ce6c812
ankur <ankursinghal.dev@gmail.com>
2018-10-21 17:56:46 +0530

    -a

--- src/components/containers/PostFeed.js
+++ src/components/containers/PostFeed.js
@@ -1,10 +1,52 @@
 import React ,{Component} from 'react';
 import { Post } from '../presentation';
+import { FlatList } from 'react-native';
 
 
 export default class PostFeed extends Component {
-    render(){
+
+    
+    _returnKey(item){
+        return item.toString();
+    }
+
+    _renderPost({item}){
         return <Post />;
+    };
+
+    render(){
+
+        
+        return( 
+          <FlatList 
+           data= {[
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                19,
+                20 ]} 
+           ketExtractor={this._returnKey}
+           renderItem={this._renderPost}
+
+           
+        />
+    );
     }
 
 }
